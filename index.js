ace36bba41554c07fd900a2cff5a644bf206601b
Dmitry <2beyva@gmail.com>
2020-05-11 11:33:38 +0000

    fix route

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 7000
 
-app.get('/', (req, res) => res.send({message: 'Hello World!'}))
+app.get('/hello', (req, res) => res.send({message: 'Hello World!'}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
