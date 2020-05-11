bae5cc57296cc0b801fd3966c6634b18014d0682
Dmitry <2beyva@gmail.com>
2020-05-11 13:16:26 +0000

    add dog response

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 7000
 
 app.get('/hello', (req, res) => res.send({message: 'Hello World!'}))
+
+app.get('/dog', (req, res) => {
+  fs.readFile(`${__dirname}/public/dog.png`, (err, data) => {
+    if (err) throw err;
+    res.write(data)
+  });
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
